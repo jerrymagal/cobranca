ede4f1f09b447cee0d8caa6fb72c7ae54b583ef7
Alexandre Guimarães Rolim <alexandreg.rolim@saude.gov.br>
2017-07-13 16:02:32 -0300

    Templates thymeleaf

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -1,10 +1,16 @@
 package com.algaworks.cobranca.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.algaworks.cobranca.enums.StatusTitulo;
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.Titulos;
 
@@ -16,14 +22,26 @@ public class TituloController {
 	private Titulos repository;
 
 	@RequestMapping("/novo")
-	public String novo() {
-		return "cadastroTitulo";
+	public ModelAndView novo() {
+		return new ModelAndView("cadastroTitulo");
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public String salvar(Titulo titulo) {
+	public ModelAndView salvar(Titulo titulo) {
 		repository.save(titulo);
-		return "cadastroTitulo";
+		ModelAndView view = new ModelAndView("cadastroTitulo");
+		view.addObject("mensagem", "Título salvo com sucesso!");
+		return view;
+	}
+	
+	@RequestMapping
+	public String pesquisar() {
+		return "pesquisaTitulo";
+	}
+	
+	@ModelAttribute("comboStatus")
+	public List<StatusTitulo> comboStatus() {
+		return Arrays.asList(StatusTitulo.values());
 	}
 	
 }
